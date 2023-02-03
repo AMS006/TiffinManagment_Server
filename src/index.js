0e8d6b39b1d07630adcc93de1b215de3e2244362
AMS006 <anassain13@gmail.com>
2023-02-03 17:44:14 +0530

    Updated

--- src/index.js
+++ src/index.js
@@ -18,12 +18,9 @@ const app = express()
 env.config();
 
 app.use(cors({
-    origin: '*', 
+    origin: ['https://tiffin-managment-client.vercel.app','http://localhost:3000'], 
     methods: ['GET', 'PUT', 'POST','DELETE'], 
-    allowedHeaders: ['Content-Type', 'Authorization', 'x-csrf-token'], 
-    credentials: true,
-    maxAge: 5000,
-    exposedHeaders: ['*', 'Authorization' ]
+    credentials: true
 }))
 app.use(express.json())
 app.use(cookieParser())
