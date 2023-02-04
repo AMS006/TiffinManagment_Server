b85ab7e22da97d44e6b6f4edb44e8e94946cb387
AMS006 <anassain13@gmail.com>
2023-02-04 12:37:53 +0530

    Updated options of cookie

--- src/utils/generateToken.js
+++ src/utils/generateToken.js
@@ -9,17 +9,18 @@ const generateToken = (res,statusCode,user,isUser) =>{
             token = user.generateJwtToken(); // Generating token for provider
             text = 'providerToken'
         }
-    const options = {
-        expires : new Date(
-            Date.now() + 5 * 24 * 60 * 60 * 1000
-        ),
-        secure:false,
-        httpOnly:true
-    }
-    if(text === "userToken")
-       return res.status(statusCode).cookie(text,token, options).json({success:true, user})
-    else
-       return res.status(statusCode).cookie(text,token, options).json({success:true, provider:user})
+        const options = {
+            expires : new Date(
+                Date.now() + 5 * 24 * 60 * 60 * 1000
+            ),
+            secure:env.ENVIRONMENT === 'LIVE',
+            sameSite: env.ENVIRONMENT === 'LIVE' ? 'none' : 'lax',
+            httpOnly:false
+        }
+        if(text === "userToken")
+            return res.status(statusCode).cookie(text,token, options).json({success:true, user})
+    
+        return res.status(statusCode).cookie(text,token, options).json({success:true, provider:user})
     }catch(error){
         return res.status(500).json({message:error.message});
     }
