8a03ec7769563928ecb9ad06442313b3216dcd4c
Anas Sain <98727007+AMS006@users.noreply.github.com>
2024-06-07 18:31:12 +0530

    Update order.js

--- src/controllers/order.js
+++ src/controllers/order.js
@@ -80,8 +80,10 @@ exports.updateOrderStatus = async(req,res) =>{
             subject = "Cancelled: Order has been Cancelled"
             message = `Hi ${req.body.provider.name} Your Order for ${req.body.food.name} has been Cancelled by ${req.body.user.name}`
             email = req.body.provider.email
+            
             const quantity = req.body.food.quantity + req.body.quantity
-            await foodModel.findByIdAndUpdate(req.body.food._id,{$set:{quantity}})
+            
+            await foodModel.findByIdAndUpdate(req.body.food._id,{$inc:{quantity}})
         }
         // await sendEmail({email,subject,message});
 
